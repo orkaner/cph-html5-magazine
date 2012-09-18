d6529777eb8f8e91e8e4da9ba2cf0defdd7c020c
naoufalmed <medouri@yahoo.fr>
2012-09-19 00:30:13 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jaywebmag::Application.config.secret_token = '6d5041f4d4379e26ed813a9c4d0f6dc7cadf7039f37d2a62c61aeea7dbe1bb018f576f02403d525140cd37087310aa1db479598da88f6a9b3b888591cc7317b0'
