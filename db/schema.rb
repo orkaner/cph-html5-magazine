bec6b5892ed1d5732d9eada9c783c40939917458
naoufalmed <medouri@yahoo.fr>
2012-10-01 13:33:34 +0200

    Fixed basic site navigation for admin module

--- db/schema.rb
+++ db/schema.rb
@@ -52,11 +52,4 @@ ActiveRecord::Schema.define(:version => 20120919111305) do
     t.datetime "updated_at", :null => false
   end
 
-  create_table "templates", :force => true do |t|
-    t.string   "name"
-    t.string   "path"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
 end
